bcd8bd7e6964a627f0402aee9c44b1edd8796ad8
Daniel Emeka Obikoru <daniel.emeka@blinqpay.io>
2026-07-31 11:55:02 +0100

    fix for meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ import { SITE_URL } from "@/lib/site"
 import Script from "next/script"
 
 const GOOGLE_TAG_ID = "AW-937925676"
+const META_PIXEL_ID = "418920915479977"
 
 
 export const metadata: Metadata = {
@@ -138,6 +139,31 @@ export default function RootLayout({
             gtag('config', '${GOOGLE_TAG_ID}');
           `}
         </Script>
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
